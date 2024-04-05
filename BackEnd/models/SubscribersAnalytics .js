bbe39fafb02a739a42c513fb5f3971acbcb14724
Ndevu12 <niyokwizerwajeanpaulelisa@gmail.com>
2024-04-05 17:54:30 +0200

    fix: fixing some errors

---
+++ BackEnd/models/SubscribersAnalytics .js
@@ -0,0 +1,61 @@
+/**
+ * This file deals with storing and handling Subscribers Analytics information.
+ */
+import { Schema as _Schema, model } from "mongoose";
+const Schema = _Schema;
+
+
+class SubscribersAnalytics {
+    constructor(){
+/**
+ * Mongoose schema for the Subscribers Analytics collection.
+ */
+const subscribersAnalyticsSchema = new Schema({
+    dateRange: {
+        type: String,
+        required: true,
+    },
+    totalSubscribers: {
+        type: Number,
+        default: 0,
+    },
+    subscriptionConversionRate: {
+        type: Number,
+        default: 0,
+    },
+    churnRate: {
+        type: Number,
+        default: 0,
+    },
+});
+    
+// Virtual for this about instance URL.
+subscribersAnalyticsSchema.virtual("url").get(function () {
+    return "/DashBoard/subscribersAnalytics/" + this._id;
+    });
+// Create Subscribers Analytics model
+this.model = model('SubscribersAnalytics', subscribersAnalyticsSchema);
+    }
+
+    createsubscribersAnalytics(){
+        return this.model.create(data);
+    }
+
+     // Method to find a comment by ID
+     findsubscribersAnalyticsById(id) {
+        return this.model.findById(id).exec();
+    }
+    // Method to update a comment
+    updatesubscribersAnalytics(id, data) {
+        return this.model.findByIdAndUpdate(id, data, { new: true }).exec();
+    }
+    // Method to delete a comment by ID
+    deletesubscribersAnalytics(id) {
+        return this.model.findByIdAndDelete(id).exec();
+    }
+    getAllsubscribersAnalytics() {
+        return this.model.find().exec();
+    }
+}
+// export Subscribers Analytics model
+export default SubscribersAnalytics;
---
+++ BackEnd/models/author.js
@@ -0,0 +1,66 @@
+/**
+ * This file handles the Author collection.
+ */
+import { Schema as _Schema, model } from "mongoose";
+const Schema = _Schema;
+/**
+ * Class representing the Author model.
+ */
+class AuthorModel {
+    constructor() {
+        const authorSchema = new Schema({
+            name: {
+                type: String,
+                required: true,
+            },
+            postId: {
+                type: Schema.ObjectId,
+                ref: "Blog", 
+                required: true
+            },
+            createdAt: {
+                type: Date,
+                required: true,
+            },
+        });
+
+        // Virtual for this about instance URL.
+        authorSchema.virtual("url").get(function () {
+            return "blog/author/" + this._id;
+            });
+        this.model = model('Author', authorSchema);
+    }
+    /**
+     * Method to create a new author document.
+     * @param data - Data for the new author.
+     * @returns Promise resolving to the created author document.
+     */
+    async createAuthor(data) {
+        const newAuthor = await this.model.create(data);
+        newAuthor.save();
+        return newAuthor;
+    }
+    /**
+     * Method to find an author document by ID.
+     * @param id - ID of the author to find.
+     * @returns Promise resolving to the found author document or null if not found.
+     */
+    async findAuthorById(id) {
+        return await this.model.findById(id).exec();
+    }
+    async updateAuthor(id, data) {
+        return await this.model.findByIdAndUpdate(id, data, { new: true }).exec();
+    }
+    /**
+     * Method to delete an Author document.
+     * @param id Author ID to delete.
+     * @returns Promise resolving to the deleted Author document, or null if not found.
+     */
+    async deleteAuthor(id) {
+        return await this.model.findByIdAndDelete(id).exec();
+    }
+    async getAllAuthors() {
+        return await this.model.find().exec();
+    }
+}
+export default  AuthorModel;
