867e0b1ec17a4974dc025ba4af2f6eb92b7d41a3
Jean Paul Elisa NIYOKWIZERWA <140616733+Ndevu12@users.noreply.github.com>
2025-06-12 17:45:57 +0200

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 const encryptedUrl = "aHR0cDovL2xvY2FsaG9zdDo2MDkwL3Yx";
 const encryptedProUrl = "aHR0cHM6Ly9teS1icmFuZC1iYWNrZW5kLWFwaXMub25yZW5kZXIuY29tL3Yx";
 
-const isProduction = false;
+const isProduction = true;
 
 function decryptUrl() {
   if (isProduction) {
